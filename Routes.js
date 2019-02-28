72bf439e3c140499eb580b0900520882dced1f4a
Mario <contacto@mariodiezgarcia.com>
2019-02-28 22:22:48 +0100

    change file

--- Routes.js
+++ Routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Navigation } from 'react-native-navigation'
 
-import App from './App'
+import Linias from './Linias'
 import Paradas from './components/Paradas'
 import Time from './components/Time'
 
 const registerScreens = () => {
-  Navigation.registerComponent('Home', () => App)
+  Navigation.registerComponent('Home', () => Linias)
   Navigation.registerComponent('Paradas', () => Paradas)
   Navigation.registerComponent('Time', () => Time)
 }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Linias from './Linias';
 import {name as appName} from './app.json';
 import { Navigation } from 'react-native-navigation';
 import registerScreens from './Routes'
